f29de8c8cf1cce0796a3178935cdf4a37ada83fa
Aaron Wiggins <apw7177@gmail.com>
2015-06-12 13:28:13 -0400

    set root

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'sessions/login' 
+  get 'sessions/login'
   post 'sessions/login'
   get 'sessions/logout'
-
+  root 'users#index'
   resources :questions
   resources :surveys
   resources :answers
