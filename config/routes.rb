6a2ef415f76ba628d19a29882cd6de2d0504afaa
Aaron Wiggins <apw7177@gmail.com>
2015-06-12 18:12:27 -0400

    small edit to README

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'sessions/login'
   post 'sessions/login'
   get 'sessions/logout'
-  root 'users#index'
+  root 'surveys#index'
   resources :questions
   resources :surveys
   resources :answers
