e35f5384bb54e298627ce5b41909530cc46cf20c
Aaron Wiggins <apw7177@gmail.com>
2015-06-12 17:53:13 -0400

    git rid of double notice message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
     private def logged_in?
-      if !User.find_by_id(session[:user_id])
+      if User.find_by_id(session[:user_id]) == nil
+        redirect_to sessions_login_path
+      elsif !User.find_by_id(session[:user_id])
         redirect_to sessions_login_path, notice: "You can't access that page."
       end
     end
