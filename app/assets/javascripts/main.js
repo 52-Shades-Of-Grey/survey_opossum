19911c12b523fa5c43f3eac561bede7de6b2c157
John Joseph <jmj1444@gmail.com>
2015-06-12 19:04:28 -0400

    made lis show up

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,9 +1,18 @@
 $(function (){
 
 	$(".btn-new-question").on('click', function () {
-		$(".new-question-modal").addClass( " show" )
+		$(".new-question-modal").addClass( "new-question-modal show" )
 	});
 
+	$(".edit_survey").on('submit', function (e) {
+		e.preventDefault();
+		var queryValue = $('#survey_questions_attributes_0_query').val();
+		var newLi = '<li class="new-question-li">' + queryValue + '<button class="question-delete">X</button></li>'
+
+		$('.question-ul').append(newLi).html();
+		$('.new-question-modal').removeClass("show");
+		$('.new-question-modal').addClass("new-question-modal hide");
+	})
 	
 
 });
