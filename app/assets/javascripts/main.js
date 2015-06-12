fdaa7e5a75d89306911f5a122d777f72087bdc56
John Joseph <jmj1444@gmail.com>
2015-06-12 19:16:11 -0400

    collecting values from form

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,17 +1,21 @@
 $(function (){
 
 	$(".btn-new-question").on('click', function () {
-		$(".new-question-modal").addClass( "new-question-modal show" )
+		$(".new-question-modal").addClass( " show" )
 	});
 
 	$(".edit_survey").on('submit', function (e) {
 		e.preventDefault();
 		var queryValue = $('#survey_questions_attributes_0_query').val();
-		var newLi = '<li class="new-question-li">' + queryValue + '<button class="question-delete">X</button></li>'
+		var required = $("#survey_questions_attributes_0_required").val();
+		var queryType = $("#survey_questions_attributes_0_query_type").val()
+		var position = $('#survey_questions_attributes_0_position').val();
+
+		var newLi = '<li class="new-question-li">' + queryValue +  required + position + '<button class="question-delete">X</button></li>'
 
 		$('.question-ul').append(newLi).html();
 		$('.new-question-modal').removeClass("show");
-		$('.new-question-modal').addClass("new-question-modal hide");
+		$('.new-question-modal').addClass(" hide");
 	})
 	
 
