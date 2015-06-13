249c41b677a30b69ddb71e51bd365a6b67e9f732
John Joseph <jmj1444@gmail.com>
2015-06-12 20:47:44 -0400

    incrementing ids

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -4,14 +4,36 @@ $(function (){
 		$(".new-question-modal").addClass( " show" )
 	});
 
+	var counter = 0;
+
 	$(".edit_survey").on('submit', function (e) {
 		e.preventDefault();
-		var queryValue = $('#survey_questions_attributes_0_query').val();
-		var required = $("#survey_questions_attributes_0_required").val();
-		var queryType = $("#survey_questions_attributes_0_query_type").val()
-		var position = $('#survey_questions_attributes_0_position').val();
 
-		var newLi = '<li class="new-question-li">' + queryValue + '<button class="question-delete">X</button></li>'
+		var query = $('#survey_questions_attributes_' + counter + '_query');
+		var required = $('#survey_questions_attributes_' + counter + '_required');
+		var queryType = $('#survey_questions_attributes_' + counter + '_query_type');
+		var position = $('#survey_questions_attributes_' + counter + '_position');
+
+		var queryVal = query.val();
+		var requiredVal = required.val();
+		var queryTypeVal = queryType.val()
+		var positionVal = position.val();
+
+		console.log(query);
+		
+
+
+		// 'survey[questions_attributes][' + counter + '][query]'
+		// 'survey[questions_attributes][' + counter + '][description]'
+		// 'survey[questions_attributes][' + counter + '][required]'
+		// 'survey[questions_attributes][' + counter + '][query_type]'
+		// 'survey[questions_attributes][' + counter + '][position]'
+
+
+		counter++;
+		console.log(counter);
+
+		var newLi = '<li class="new-question-li">' + queryVal + '<button class="question-delete">X</button></li>'
 
 		$('.question-ul').append(newLi).html();
 		
