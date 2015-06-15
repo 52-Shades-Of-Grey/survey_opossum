bae7dd3dcfee2ce7cbbf7444dd3831189a3f938a
Aaron Wiggins <apw7177@gmail.com>
2015-06-15 10:31:19 -0400

    fix login_page button display

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def login_button
+    if params[:action] != "get" && params[:controller] != "sessions"
+      link_to_unless_current("Login Page", sessions_login_path)
+    end
+  end
+
+  def logout_button
+    link_to "Logout", sessions_logout_path if session[:user_id]
+  end
 end
